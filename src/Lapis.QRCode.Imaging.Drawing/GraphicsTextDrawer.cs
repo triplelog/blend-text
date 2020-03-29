a4aeb04fecc8b10889ff603b92cab3f19f0225a0
triplelog <robmwilc@gmail.com>
2020-03-29 18:40:19 -0400

    First

--- src/Lapis.QRCode.Imaging.Drawing/GraphicsTextDrawer.cs
+++ src/Lapis.QRCode.Imaging.Drawing/GraphicsTextDrawer.cs
@@ -71,7 +71,7 @@ namespace Lapis.QRCode.Imaging.Drawing
                         else {
                         	var x = MarginL + c;
                             var y = MarginT + r;
-                            var imgC = bmp.GetPixel(x,y);
+                            int imgC = bmp.GetPixel(x,y);
 							int re = (imgC & 0xFF0000) >> 16;
 							int gr = (imgC & 0xFF00) >> 8;
 							int bl = imgC & 0xFF;
