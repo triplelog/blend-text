e214a229df7f1358a6ce2d524f16176250457f93
triplelog <robmwilc@gmail.com>
2020-03-30 19:38:59 -0400

    First

--- src/qr-art/Program.cs
+++ src/qr-art/Program.cs
@@ -70,6 +70,8 @@ namespace Lapis.QrArt
 						
 						string fontVal = getFont(fontOpt.Value());
 						Font font = new Font(fontVal, fontSize);
+        				Console.WriteLine(font.FontFamily);
+        				
         				
         				Stopwatch stopWatch = new Stopwatch();
         				stopWatch.Start();
@@ -83,7 +85,7 @@ namespace Lapis.QrArt
 							bmp = Bitmap.FromFile(imageArg.Value) as Bitmap;
 							Graphics graph1 = Graphics.FromImage(bmp);
 							string measureString = contentArg.Value;
-							Console.WriteLine(font.FontFamily);
+							
 							SizeF stringSize = new SizeF();
 							stringSize = graph1.MeasureString(measureString, font);
 							Console.WriteLine("width "+stringSize.Width + " height "+ stringSize.Height + "fSize "+fontSize);
@@ -194,13 +196,65 @@ namespace Lapis.QrArt
         }
         private static string getFont(string input_str)
         {
+        	string outFont = input_str;
+        	int minLev = 100;
+        	int myLev = 100;
         	FontFamily[] ffArray = FontFamily.Families;
 			foreach (FontFamily ff in ffArray)
 			{
-				//Add ff.Name to your drop-down list
-				//Console.WriteLine(ff.Name);
+				myLev = LevenshteinDistance(input_str, ff.Name)
+				if (myLev < minLev){
+					minLev = myLev;
+					outFont = ff.Name;
+				}
 			}
-            return input_str;
+			Console.WriteLine("original" + input_str + " found "+ outFont);
+            return outFont;
         }
+        
+        private static int LevenshteinDistance(string s, string t)
+		{
+			int n = s.Length;
+			int m = t.Length;
+			int[,] d = new int[n + 1, m + 1];
+
+			// Step 1
+			if (n == 0)
+			{
+				return m;
+			}
+
+			if (m == 0)
+			{
+				return n;
+			}
+
+			// Step 2
+			for (int i = 0; i <= n; d[i, 0] = i++)
+			{
+			}
+
+			for (int j = 0; j <= m; d[0, j] = j++)
+			{
+			}
+
+			// Step 3
+			for (int i = 1; i <= n; i++)
+			{
+				//Step 4
+				for (int j = 1; j <= m; j++)
+				{
+					// Step 5
+					int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+
+					// Step 6
+					d[i, j] = Math.Min(
+						Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+						d[i - 1, j - 1] + cost);
+				}
+			}
+			// Step 7
+			return d[n, m];
+		}
     }
 }
