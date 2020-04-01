05880af34f5745163710c47f9dc35224e3e72eb2
triplelog <robmwilc@gmail.com>
2020-04-01 14:25:27 -0400

    First

--- server/bt-server.js
+++ server/bt-server.js
@@ -91,8 +91,7 @@ wss.on('connection', function connection(ws) {
   var myTimeout;
   var imgid = parseInt(crypto.randomBytes(50).toString('hex'),16).toString(36).substr(2, 12);
   var outSrc = 'out/'+imgid+'.png';
-  var inSrc = 'images/in/'+imgid+'.png';
-  inSrc = "test.jpg";
+  var inSrc = 'images/in/'+imgid+'.jpg';
   var imgIndex = 0;
   ws.on('message', function incoming(message) {
   	console.log(message);
