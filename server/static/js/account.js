78c478278352772e98c7673e5c1c08a7a3a27090
triplelog <robmwilc@gmail.com>
2020-04-03 19:58:12 -0400

    First

---
+++ server/static/js/account.js
@@ -0,0 +1,16 @@
+var ws = new WebSocket('wss://matherrors.com:8080');
+ws.onopen = function(evt) {
+	var jsonmessage = {'type':'key'};
+	jsonmessage.message = tkey;
+	ws.send(JSON.stringify(jsonmessage));
+}
+ws.onmessage = function(evt){
+	var dm = JSON.parse(evt.data);
+
+}
+
+function copyFormula(id){
+	var jsonmessage = {'type':'copyFormula'};
+	jsonmessage.message = id;
+	ws.send(JSON.stringify(jsonmessage));
+}
