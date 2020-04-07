599628834b0699d8276d7ab83c3fa83e0d7dfe39
triplelog <robmwilc@gmail.com>
2020-04-07 12:14:16 -0400

    First

--- src/Lapis.QRCode.Art/QRArtCreator.cs
+++ src/Lapis.QRCode.Art/QRArtCreator.cs
@@ -66,7 +66,7 @@ namespace Lapis.QRCode.Art
         		
         		state.DoString (arrCode);
 				var arrFunc = state ["DistanceFunc"] as LuaFunction;
-				var res = arrFunc.Call (arr2,10);
+				int[] res = arrFunc.Call (arr2,10) as int[];
         		//Console.WriteLine("Luafunc test return "+res[0]);
         		/*string scriptCode = @"function DistanceFunc (d,maxD)
         		return -10
@@ -179,7 +179,7 @@ namespace Lapis.QRCode.Art
 							
 						}*/
 					}
-					res = arrFunc.Call (arr, maxD);
+					res = arrFunc.Call (arr, maxD) as int[];
 					if (i== 0){
 						Console.WriteLine("Luafunc test return "+res[0][0]);
 					}
