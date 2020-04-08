657982d7c0baf68f6a9e68479775236eff6db2ed
triplelog <robmwilc@gmail.com>
2020-04-08 09:08:26 -0400

    First

--- src/Lapis.QRCode.Imaging.Drawing/GraphicsTextDrawer.cs
+++ src/Lapis.QRCode.Imaging.Drawing/GraphicsTextDrawer.cs
@@ -61,6 +61,7 @@ namespace Lapis.QRCode.Imaging.Drawing
 				
                 for (var r = startR; r < THeight && r + MarginT < bmp.Height; r += CellWidth)
                 {
+                	Console.WriteLine(r);
                     for (var c = startC; c < TWidth && c + MarginL < bmp.Width; c += CellWidth)
                     {
                         if (tripMatrix[r, c] == 0)
