ed6f13ae454bc08efb90ca343fdcf6729cdcbeb8
triplelog <robmwilc@gmail.com>
2020-04-07 22:47:29 -0400

    First

--- src/Lapis.QRCode.Imaging/IBitMatrixDrawer.cs
+++ src/Lapis.QRCode.Imaging/IBitMatrixDrawer.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Drawing;
-using System.Drawing.Imaging;
-using System.Drawing.Bitmap;
+using System.Drawing.Common;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
