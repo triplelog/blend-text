2363bdf80d2999a30cff0f6ec60e7f8e410b0535
triplelog <robmwilc@gmail.com>
2020-04-07 22:49:54 -0400

    First

--- src/Lapis.QRCode.Imaging/IBitMatrixDrawer.cs
+++ src/Lapis.QRCode.Imaging/IBitMatrixDrawer.cs
@@ -7,7 +7,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Lapis.QRCode.Encoding;
 
-namespace Lapis.QRCode.Imaging
+namespace Lapis.QRCode.Imaging.Drawing
 {
     public interface IBitMatrixDrawer
     {
