e46af0b1da4550ce08b494164ccd2cbd8af0bdc6
triplelog <robmwilc@gmail.com>
2020-04-09 21:04:13 -0400

    First

--- server/bt-server.js
+++ server/bt-server.js
@@ -282,117 +282,184 @@ wss.on('connection', function connection(ws) {
 	}
 	
 	
+	if (dm.type !="gradient"){
+		//Start creating image if made it this far
+		if (username != ''){
+			console.log('username: '+username);
+		}
+		//console.log(dm);
+		if (!dm.fontSize || dm.textFormula == "" || dm.blurFormula == ""){
+			return;
+		}
+		// add more checks
 	
-	//Start creating image if made it this far
-	if (username != ''){
-		console.log('username: '+username);
-	}
-  	//console.log(dm);
-  	if (!dm.fontSize || dm.textFormula == "" || dm.blurFormula == ""){
-  		return;
-  	}
-  	// add more checks
-  	
 
-	//download or upload file to 'inputs/imgid'+fileExt and set inputSrc
-	//var inSrc = 'test.jpg';
+		//download or upload file to 'inputs/imgid'+fileExt and set inputSrc
+		//var inSrc = 'test.jpg';
 	
-  	var execCmd = '../src/qr-art/bin/Debug/netcoreapp3.1/publish/qr-art "'+dm.text+'" '+inSrc+' png static/'+outSrc;
-  	execCmd += ' -s '+dm.fontSize;
-  	execCmd += ' -x '+dm.locX;
-  	execCmd += ' -y '+dm.locY;
-  	execCmd += ' -r '+dm.blurRadius;
-  	if (dm.width && dm.width != -1){
-  		execCmd += ' -w '+dm.width;
-  	}
-  	
-  	var workspace = new Blockly.Workspace();
-  	var wxml = Blockly.Xml.textToDom(dm.blurFormula);
-  	Blockly.Xml.domToWorkspace(wxml, workspace);
-  	var code = Blockly.Lua.workspaceToCode(workspace);
-  	console.log(code);
-  	if (dm.blurType == 'hsl'){
-		luaBlurFormula = `function ScriptFunc (d,h,s,l)
-		`+code+`
-		return h,s,l
-		end
-		`
+		var execCmd = '../src/qr-art/bin/Debug/netcoreapp3.1/publish/qr-art "'+dm.text+'" '+inSrc+' png static/'+outSrc;
+		execCmd += ' -s '+dm.fontSize;
+		execCmd += ' -x '+dm.locX;
+		execCmd += ' -y '+dm.locY;
+		execCmd += ' -r '+dm.blurRadius;
+		if (dm.width && dm.width != -1){
+			execCmd += ' -w '+dm.width;
+		}
 	
-		execCmd += ' -b "testBlur"';
-		execCmd += ' -B hsl';
-  	}
-  	else {
-  		luaBlurFormula = `function ScriptFunc (d,r,g,b)
-		`+code+`
-		return r,g,b
-		end
-		`
+		var workspace = new Blockly.Workspace();
+		var wxml = Blockly.Xml.textToDom(dm.blurFormula);
+		Blockly.Xml.domToWorkspace(wxml, workspace);
+		var code = Blockly.Lua.workspaceToCode(workspace);
+		console.log(code);
+		if (dm.blurType == 'hsl'){
+			luaBlurFormula = `function ScriptFunc (d,h,s,l)
+			`+code+`
+			return h,s,l
+			end
+			`
 	
-		execCmd += ' -b "testBlur"';
-		execCmd += ' -B rgb';
-  	}
-  	
-  	workspace = new Blockly.Workspace();
-  	wxml = Blockly.Xml.textToDom(dm.textFormula);
-  	Blockly.Xml.domToWorkspace(wxml, workspace);
-  	code = Blockly.Lua.workspaceToCode(workspace);
-  	console.log(code);
-  	if (dm.textType == 'hsl'){
-		luaTextFormula = `function TextFunc (d,h,s,l)
-		`+code+`
-		return h,s,l
-		end
-		`
+			execCmd += ' -b "testBlur"';
+			execCmd += ' -B hsl';
+		}
+		else {
+			luaBlurFormula = `function ScriptFunc (d,r,g,b)
+			`+code+`
+			return r,g,b
+			end
+			`
 	
-		execCmd += ' -c "testText"';
-		execCmd += ' -C hsl';
-  	}
-  	else {
-  		luaTextFormula = `function TextFunc (d,r,g,b)
-		`+code+`
-		return r,g,b
-		end
-		`
+			execCmd += ' -b "testBlur"';
+			execCmd += ' -B rgb';
+		}
 	
-		execCmd += ' -c "testText"';
-		execCmd += ' -C rgb';
-  	}
+		workspace = new Blockly.Workspace();
+		wxml = Blockly.Xml.textToDom(dm.textFormula);
+		Blockly.Xml.domToWorkspace(wxml, workspace);
+		code = Blockly.Lua.workspaceToCode(workspace);
+		console.log(code);
+		if (dm.textType == 'hsl'){
+			luaTextFormula = `function TextFunc (d,h,s,l)
+			`+code+`
+			return h,s,l
+			end
+			`
+	
+			execCmd += ' -c "testText"';
+			execCmd += ' -C hsl';
+		}
+		else {
+			luaTextFormula = `function TextFunc (d,r,g,b)
+			`+code+`
+			return r,g,b
+			end
+			`
+	
+			execCmd += ' -c "testText"';
+			execCmd += ' -C rgb';
+		}
 
-  	if (dm.font.indexOf('"')==-1 && dm.font.indexOf(' ')>0){
-  		execCmd += ' -f "'+dm.font+'"';
-  	}
-  	else {
-  		execCmd += ' -f '+dm.font;
-  	}
-  	
-  	execCmd += ' -t '+dm.type;
-  	
-  	if (dm.threshold){
-  		execCmd += ' -l '+dm.threshold;
-  	}
-  	
-  	var luaDistanceFormula = `function DistanceFunc (d,maxD)
-        		return (10*d-15*maxD)*2/maxD
-        		end`;
-    execCmd += ' -d "'+'test'+'Distance"';
-    
-  	console.log(execCmd);
-  	
-  	if (newCreation && username != ''){
-		//Add a Check that there does not exist a creation of that name already.
-		User.updateOne({ username: username }, {$push: {"creations": outSrc}}, function(err, result) {});
-		newCreation = false;
-	}
+		if (dm.font.indexOf('"')==-1 && dm.font.indexOf(' ')>0){
+			execCmd += ' -f "'+dm.font+'"';
+		}
+		else {
+			execCmd += ' -f '+dm.font;
+		}
+	
+		execCmd += ' -t '+dm.type;
+	
+		if (dm.threshold){
+			execCmd += ' -l '+dm.threshold;
+		}
+	
+		var luaDistanceFormula = `function DistanceFunc (d,maxD)
+					return (10*d-15*maxD)*2/maxD
+					end`;
+		execCmd += ' -d "'+'test'+'Distance"';
+	
+		console.log(execCmd);
+	
+		if (newCreation && username != ''){
+			//Add a Check that there does not exist a creation of that name already.
+			User.updateOne({ username: username }, {$push: {"creations": outSrc}}, function(err, result) {});
+			newCreation = false;
+		}
 		
-  	if (myTimeout){
-		clearTimeout(myTimeout);
-		myTimeout = setTimeout(function(){ runCommand(ws,execCmd,outSrc,imgIndex, luaBlurFormula, luaTextFormula, luaDistanceFormula); }, 1000);
-	}
-	else {
-		myTimeout = setTimeout(function(){ runCommand(ws,execCmd,outSrc,imgIndex, luaBlurFormula, luaTextFormula, luaDistanceFormula); }, 1000);
-	}
-	imgIndex++;
-  	
+		if (myTimeout){
+			clearTimeout(myTimeout);
+			myTimeout = setTimeout(function(){ runCommand(ws,execCmd,outSrc,imgIndex, luaBlurFormula, luaTextFormula, luaDistanceFormula); }, 1000);
+		}
+		else {
+			myTimeout = setTimeout(function(){ runCommand(ws,execCmd,outSrc,imgIndex, luaBlurFormula, luaTextFormula, luaDistanceFormula); }, 1000);
+		}
+		imgIndex++;
+  	}
+  	else if (dm.type =="gradient") {
+  		//Start creating image if made it this far
+		if (username != ''){
+			console.log('username: '+username);
+		}
+		//console.log(dm);
+		if (!2==2){
+			return;
+		}
+		// add more checks
+	
+	
+		var execCmd = '../src/qr-art/bin/Debug/netcoreapp3.1/publish/qr-art "dummyText" '+inSrc+' png static/'+outSrc;
+
+		execCmd += ' -x '+dm.locX;
+		execCmd += ' -y '+dm.locY;
+	
+		var workspace = new Blockly.Workspace();
+		var wxml = Blockly.Xml.textToDom(dm.blurFormula);
+		Blockly.Xml.domToWorkspace(wxml, workspace);
+		var code = Blockly.Lua.workspaceToCode(workspace);
+		console.log(code);
+		if (dm.blurType == 'hsl'){
+			luaBlurFormula = `function ScriptFunc (d,h,s,l)
+			`+code+`
+			return h,s,l
+			end
+			`
+	
+			execCmd += ' -b "testBlur"';
+			execCmd += ' -B hsl';
+		}
+		else {
+			luaBlurFormula = `function ScriptFunc (d,r,g,b)
+			`+code+`
+			return r,g,b
+			end
+			`
+	
+			execCmd += ' -b "testBlur"';
+			execCmd += ' -B rgb';
+		}
+	
+	
+		execCmd += ' -t '+dm.type;
+	
+		if (dm.threshold){
+			execCmd += ' -l '+dm.threshold;
+		}
+	
+		console.log(execCmd);
+	
+		if (newCreation && username != ''){
+			//Add a Check that there does not exist a creation of that name already.
+			User.updateOne({ username: username }, {$push: {"creations": outSrc}}, function(err, result) {});
+			newCreation = false;
+		}
+		
+		if (myTimeout){
+			clearTimeout(myTimeout);
+			myTimeout = setTimeout(function(){ createGradient(ws,execCmd,outSrc,imgIndex, luaBlurFormula); }, 1000);
+		}
+		else {
+			myTimeout = setTimeout(function(){ createGradient(ws,execCmd,outSrc,imgIndex, luaBlurFormula); }, 1000);
+		}
+		imgIndex++;
+  	}
   	
   	
   });
@@ -423,6 +490,30 @@ function runCommand(ws,execCmd,outSrc,imgIndex, luaBlurFormula, luaTextFormula, 
 				}
 			});
 		});
+  	});
+	
+}
+function createGradient(ws,execCmd,outSrc,imgIndex, luaBlurFormula) {
+	var formulaName = 'test';
+	fs.writeFile("formulas/"+formulaName+"Blur.txt", luaBlurFormula, function(err) {
+		if (err){
+
+		}
+		else {
+			exec(execCmd, (error, stdout, stderr) => {
+				if (error) {
+					console.log(`error: ${error.message}`);
+					return;
+				}
+				if (stderr) {
+					console.log(`stderr: ${stderr}`);
+					return;
+				}
+				console.log(`stdout: ${stdout}`);
+				var jsonmessage = {'src':outSrc+'?'+imgIndex};
+				ws.send(JSON.stringify(jsonmessage));
+			});
+		}
   	});
 	
 }
