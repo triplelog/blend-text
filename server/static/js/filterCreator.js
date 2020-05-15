4657d58dc2380050ff411d7298a8050a46a83912
triplelog <robmwilc@gmail.com>
2020-05-15 09:46:14 -0400

    First

--- server/static/js/filterCreator.js
+++ server/static/js/filterCreator.js
@@ -282,6 +282,16 @@ function addFilter() {
 }
 document.getElementById('addFilter').querySelector('button').addEventListener('click',addFilter);
 
+dragula([document.getElementById('filterList')])
+  .on('drag', function (el) {
+    //el.className = el.className.replace('ex-moved', '');
+  }).on('drop', function (el) {
+    //el.className += ' ex-moved';
+  }).on('over', function (el, container) {
+    //container.className += ' ex-over';
+  }).on('out', function (el, container) {
+    //container.className = container.className.replace('ex-over', '');
+  });
 
 
 
