355071b8aefe6c03e723438905e7d40fd9465c24
triplelog <robmwilc@gmail.com>
2020-05-15 21:23:00 -0400

    First

--- src/qr-art/Program.cs
+++ src/qr-art/Program.cs
@@ -273,7 +273,7 @@ namespace Lapis.QrArt
         				
         				if (textDrawer.Type == "filter"){ 
 						
-							textDrawer.HashSize = 5;
+							textDrawer.HashSize = 3;
 							textDrawer.CellWidth = 2;
 							
 							
