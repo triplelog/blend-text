2074e134fa9d03d15df3efe537fc0da63412e544
triplelog <robmwilc@gmail.com>
2020-05-20 20:30:55 -0400

    First

--- server/static/js/account.js
+++ server/static/js/account.js
@@ -186,12 +186,3 @@ document.getElementById('imgSrc').style.display = 'inline-block';
 document.getElementById('imgUrl').style.display = 'none';
 document.getElementById('imgDrag').style.display = 'none';
 
-function toggleAddImage() {
-	var el = document.getElementById('addImage');
-	if (el.style.display == 'none'){
-		el.style.display = 'block';
-	}
-	else {
-		el.style.display = 'none';
-	}
-}
