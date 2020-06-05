1018b9a3447cdde998f577edcd6be46900d35733
triplelog <robmwilc@gmail.com>
2020-06-05 16:21:04 -0400

    First

--- src/qr-art/Program.cs
+++ src/qr-art/Program.cs
@@ -166,7 +166,7 @@ namespace Lapis.QrArt
 										newSize = oldSize * widthout * bmp.Width / 100 / twidth;
 										if (newSize > oldSize * bmp.Height / theight ){
 											newSize = oldSize * bmp.Height / theight;
-											oldSize = newSize;
+											//oldSize = newSize;
 										}
 										font = new Font(fontVal, newSize );
 										stringSize = graph1.MeasureString(measureString, font);
