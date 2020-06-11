90500edb68527e2a12d6f919a4f57111ec2e0604
triplelog <robmwilc@gmail.com>
2020-06-11 16:20:32 -0400

    First

--- src/Lapis.QRCode.Art/QRArtCreator.cs
+++ src/Lapis.QRCode.Art/QRArtCreator.cs
@@ -58,7 +58,7 @@ namespace Lapis.QRCode.Art
             	
         		
         		//string scriptCode = DistanceFormula;
-        		string scriptCode = `function DistanceFunc (d,maxD)\nreturn -200 - ((99*d-199*maxD)/maxD)\nend`;
+        		string scriptCode = "function DistanceFunc (d,maxD)\nreturn -200 - ((99*d-199*maxD)/maxD)\nend";
         		if (scriptCode == ""){
         			scriptCode = "function DistanceFunc (d,maxD)\nreturn -30\nend";
         		}
